095aa8327679df6f8a142ef5ce2ccd3b6967d281
joaolucaspontes <joao.lucas98@live.com>
2025-08-18 22:59:32 -0300

    User entity and resources

---
+++ src/main/java/com/joaolucas/cursospring/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.joaolucas.cursospring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaolucas.cursospring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	   User u = new User(1L, "Maria", "maria@gmail.com", "2192989329", "321435");
+	   return ResponseEntity.ok().body(u);
+	}
+}
